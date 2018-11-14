87aded22a08fd72105a1253f73ba6b727595d8c1
Srinivas <gino1610@gmail.com>
2018-11-13 21:14:47 -0800

    added description/comments

--- basic/simple_calculator.cpp
+++ basic/simple_calculator.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+/*
+
+Write a program and call it calc.cpp which is the basic calculator and receives three values from input via keyboard.
+The first value as an operator (Op1) should be a char type and one of (+, -, *, /, s) characters with the following meanings:
+‘+’ for addition (num1 + num2)
+‘-’ for subtraction (num1 – num2)
+‘*’ for multiplication (num1 * num2)
+‘/’ for division (num1 / num2)
+‘s’ for swap
+Program should receive another two operands (Num1, Num2) which could be float or integer.
+The program should apply the first given operator (Op1) into the operands (Num1, Num2) and prints the relevant results with related messages in the screen.
+Swap operator exchanges the content (swap) of two variables, for this task you are not allowed to use any further variables (You should use just two variables to swap).
+
+*/
+
 int main() 
 {
     const string appVersion = "1.00.000";
